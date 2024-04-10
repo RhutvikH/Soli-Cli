8ff46e8a2a6e07c8cdf26ac1dee89b3e71fc4133
RhutvikH <rhutvikhegde@gmail.com>
2024-04-10 21:12:05 +0530

    Basic gameplay

--- SoliGame.py
+++ SoliGame.py
@@ -50,7 +50,7 @@ class Column:
         if not self.isEmpty:
             self.cards = self.cards[:-1]
             self.numCards-=1
-            self.cards[-1].cardFlip()
+            self.cards[-1].cardFlip() if self.cards else None
         if self.numCards == 0:
             self.isEmpty = True
 
@@ -92,21 +92,29 @@ class Game_Play:
         print(self.deck, "\n", self.withdraw_deck, "\n", len(self.withdraw_deck), "\n", self.columns)
     
     def moveCard(self, fcolumnNum, cardNumber, columnNum):
-        if self.checkValidity(self.columns[fcolumnNum].cards[cardNumber], self.columns[columnNum].cards[-1]):    
-            self.columns[columnNum].extend(self.columns[fcolumnNum][cardNumber:])
-            self.columns[fcolumnNum] = self.columns[fcolumnNum][:cardNumber]
+        if self.checkValidity(self.columns[columnNum].cards[cardNumber], self.columns[fcolumnNum].cards[-1]):    
+            self.columns[columnNum].cards.extend(self.columns[fcolumnNum].cards[cardNumber:])
+            self.columns[fcolumnNum].cards = self.columns[fcolumnNum].cards[:cardNumber]
+            self.columns[fcolumnNum].deleteCard()
+            # self.columns[fcolumnNum].cards[-1].cardFlip() if self.columns[fcolumnNum].cards else None
         else:
             print("\nInvalid Move\n")
         
     def useDrawD(self, columnNum, j):
         self.columns[columnNum].addCard(self.withdraw_deck.pop(j))
-
+    
+    def nextWD(self):
+        if len(self.withdraw_deck) > 0:
+            wdc = self.withdraw_deck.pop()
+            self.withdraw_deck.insert(0, wdc)
+        else:
+            print("\nNo more cards in the draw pile\n")
     def toFoundation(self, columnNum):
         card = self.columns[columnNum].cards[-1]
         if self.checkFValidity(card):
             self.foundations[card.suit].append(card) 
             self.columns[columnNum].deleteCard()
-            self.columns[columnNum].cards[-1].cardFlip()
+            # self.columns[columnNum].cards[-1].cardFlip()
         else: 
             print("\nInvalid Move\n")
             
@@ -122,14 +130,14 @@ class Game_Play:
     def printGame(self):
         print("Draw Pile: ", end="")
         if len(self.withdraw_deck) > 0:
-            print(self.withdraw_deck[-1])
+            print(self.withdraw_deck[-1].suit, self.withdraw_deck[-1].number)
         else:
             print("Empty")
         
         print("Foundations: ", end="")
-        for foundation in self.foundations:
+        for foundation in self.foundations.values():
             if len(foundation) > 0:
-                print(foundation[-1], end=" ")
+                print("|",foundation[0].suit,foundation[0].number,"|", end=" ")
             else:
                 print("Empty", end=" ")
         print()
@@ -139,7 +147,7 @@ class Game_Play:
         for i in range(max_column_length):
             for column in self.columns:
                 if i < len(column.cards):
-                    print(column.cards[i].suit+" "+column.cards[i].number+" "*(15-len(column.cards[i].suit)-len(str(column.cards[i].number)))+"|", end=" ")
+                    print(column.cards[i].suit+" "+column.cards[i].number+" "*(15-len(column.cards[i].suit)-len(str(column.cards[i].number)))+"|", end=" ") if column.cards[i].flipped else print("???"+" "+"?"+" "*11+"|", end=" ")
                 else:
                     print(" "*16+"|", end=" ")
             print()
@@ -149,3 +157,18 @@ a = Game_Play(7)
 a.fillColumns()
 a.toFoundation(1)
 a.printGame()
+
+while True:
+    uinp = input("Enter your move: ").split(">")
+    if len(uinp)==1 and uinp[0].lower()=='w':
+        a.nextWD()
+    elif uinp[0].lower()=="w" and len(uinp)!=1:
+        a.useDrawD(int(uinp[1]), -1)
+    elif uinp[1].lower()=="f" and len(uinp[1])==1:
+        a.toFoundation(int(uinp[0]))
+    elif len(uinp[0].lower())!=1:
+        a.moveCard(int(ord(uinp[0][0].upper())), int(uinp[0][1:]), int(uinp[1]))
+
+    else:
+        a.moveCard(int(uinp[0]), -1, int(uinp[1]))
+    a.printGame()
